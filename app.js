90a1f3b9a938926f13aa8b63db02077fee1566aa
Jake Garlick <jacobgarlick@msn.com>
2019-11-22 13:08:47 -0700

    looking into individual key assignment in order to add seperate delete and edit function

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ pushNewNote = () => {
     allNotes.push(newItem.value)
 }
 
+pushOldNote = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        allNotes.push(localStorage.getItem[i])
+    }
+    console.log(allNotes.length)
+}
+
 eraseText = () => {
     document.getElementById("new-note").value = "";
 }
@@ -39,6 +46,9 @@ setNotes = () => {
     let onScreen = document.getElementById("all-notes");
     let newOnScreen = document.createElement("p");
     newOnScreen.appendChild(document.createTextNode(myNotes));
+    let button = document.createElement("button")
+    button.innerHTML = allNotes.length
+    newOnScreen.appendChild(button)
     onScreen.appendChild(newOnScreen)
 }
 
@@ -48,8 +58,41 @@ getAllNotes = () => {
         let allScreen = document.getElementById("all-notes");
         let newAllScreen = document.createElement("p");
         newAllScreen.appendChild(document.createTextNode(getAll));
+        let button = document.createElement("button")
+        button.innerHTML = localStorage.key([i])
+        newAllScreen.appendChild(button)
         allScreen.appendChild(newAllScreen)
     }
 }
 
+pushOldNote();
 getAllNotes();
+
+
+
+
+// $("#crystal-color1").on("click", function () {
+//     var valueCrystal = ($(this).attr("data-crystalvalue"));
+//     valueCrystal = parseInt(crystalNumber);
+//     roundScore += valueCrystal;
+//     console.log("value crystal " + valueCrystal);
+//     $("#round-score").text(roundScore);
+//     checkIfWon();
+// });
+
+// // Inital Crystal Values are 0
+// var crystalNumber = 0;
+// var crystalNumber2 = 0;
+// var crystalNumber3 = 0;
+// var crystalNumber4 = 0;
+
+// var scoreTarget = $("#number-to-guess").text(targetScore)
+
+// // Functions
+// // Generates new random number each game, to try to keep wide range of potential values, each crystalNumber is calculated at a progressive value
+// function generateRandomNumber() {
+//     crystalNumber = (Math.floor(Math.random() * 4) + 1);
+//     crystalNumber2 = (Math.floor(Math.random() * 7) + 1);
+//     crystalNumber3 = (Math.floor(Math.random() * 19) + 1);
+//     crystalNumber4 = (Math.floor(Math.random() * 11) + 1);
+// };
