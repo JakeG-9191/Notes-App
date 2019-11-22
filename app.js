194366b31a47b2457ea9f683de49a647ecdcc91c
Jake Garlick <jacobgarlick@msn.com>
2019-11-22 12:03:41 -0700

    added bootstrap, personal styling, build in logic to load previous notes and clear all notes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ calcNotes = () => {
     totalNotes = allNotes.length + 1;
     pushNewNote();
     addNewNote();
-    allAppend();
+    setNotes();
 }
 
 pushNewNote = () => {
@@ -19,20 +19,32 @@ addNewNote = () => localStorage.setItem(totalNotes , newItem.value)
 
 clearAll = () => localStorage.clear();
 
-clearSection = () => document.getElementById("all-notes").value("")
-
 clearClear = () => {
     clearAll();
-    clearSection();
+    location.reload()
 }
 
 document.getElementById("submit").onclick = () => { event.preventDefault(), calcNotes() }
 document.getElementById("remove-all").onclick = () => { clearClear() }
 
-const allAppend = () => {
-    let node = document.getElementById("all-notes");
-    let newNode = document.createElement("p");
-    newNode.appendChild(document.createTextNode(newItem.value
-    ));
-    node.appendChild(newNode);
+setNotes = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        myNotes = localStorage.getItem([i + 1]);
+    }
+    let onScreen = document.getElementById("all-notes");
+    let newOnScreen = document.createElement("p");
+    newOnScreen.appendChild(document.createTextNode(myNotes));
+    onScreen.appendChild(newOnScreen)
 }
+
+getAllNotes = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        getAll = localStorage.getItem([i + 1]);
+        let allScreen = document.getElementById("all-notes");
+        let newAllScreen = document.createElement("p");
+        newAllScreen.appendChild(document.createTextNode(getAll));
+        allScreen.appendChild(newAllScreen)
+    }
+}
+
+getAllNotes();
