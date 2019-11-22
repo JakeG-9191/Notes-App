569d031f10baa59333993d24e8011e2cb0c782e0
Jake Garlick <jacobgarlick@msn.com>
2019-11-21 18:39:19 -0700

    various changes to try to get delete to work proper

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let newItem = document.getElementById("new-note");
 
-let allNotes = ["k", "l", "m"]
+let allNotes = []
 
 let totalNotes = 0
 
@@ -8,6 +8,7 @@ calcNotes = () => {
     totalNotes = allNotes.length + 1;
     pushNewNote();
     addNewNote();
+    allAppend();
 }
 
 pushNewNote = () => {
@@ -18,5 +19,20 @@ addNewNote = () => localStorage.setItem(totalNotes , newItem.value)
 
 clearAll = () => localStorage.clear();
 
+clearSection = () => document.getElementById("all-notes").value("")
+
+clearClear = () => {
+    clearAll();
+    clearSection();
+}
+
 document.getElementById("submit").onclick = () => { event.preventDefault(), calcNotes() }
-document.getElementById("remove-all").onclick = () => { clearAll() }
+document.getElementById("remove-all").onclick = () => { clearClear() }
+
+const allAppend = () => {
+    let node = document.getElementById("all-notes");
+    let newNode = document.createElement("p");
+    newNode.appendChild(document.createTextNode(newItem.value
+    ));
+    node.appendChild(newNode);
+}
