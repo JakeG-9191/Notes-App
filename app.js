7c67d1248b4b49155b5d783af1c57491dbbf5217
JakeG-9191 <jacobgarlick@msn.com>
2020-08-04 13:15:53 -0600

    reworked map of original array with notes to map new notes, fixed issue with data value for old notes being retrieved from local storage

--- app.js
+++ app.js
@@ -11,24 +11,16 @@ let newItem = document.getElementById('new-note');
 
 let allNotes = [];
 
-let totalNotes = 0;
-
 calcNotes = () => {
-  totalNotes = allNotes.length + 1;
   pushNewNote();
   setNotes();
   eraseText();
-  pushOldNote();
 };
 
 pushNewNote = () => {
   allNotes.push(newItem.value);
   localStorage.setItem(1, allNotes);
   eraseText();
-};
-
-pushOldNote = () => {
-  console.log(allNotes.length);
 };
 
 eraseText = () => {
@@ -43,50 +35,46 @@ clearAll = () => {
 
 clearThisNote = () => {
   $('.btn-danger').on('click', function () {
-    var valueNum = $(this).attr('data-value');
+    const valueNum = $(this).attr('data-value');
     console.log(valueNum);
-    console.log(allNotes);
-    // console.log(window.localStorage.getItem(window.localStorage.key(valueNum)));
-    // allNotes.splice(valueNum - 1, 1);
-    // getAllNotes();
-    // setNotes();
-    // localStorage.removeItem(parseInt(valueNum));
-    // location.reload();
+    // allNotes.splice(valueNum, 1);
   });
 };
 
+let noteTally;
+
 setNotes = () => {
-  for (let i = 0; i < allNotes.length; i++) {
-    myNotes = allNotes[i];
-  }
-  let onScreen = document.getElementById('all-notes');
-  let newOnScreen = document.createElement('p');
-  newOnScreen.className = 'my-little-note';
-  newOnScreen.appendChild(document.createTextNode(myNotes));
-  let button = document.createElement('button');
-  // button.id = `app-button-${localStorage.length}`;
-  button.className = 'btn btn-danger';
-  button.setAttribute('data-value', allNotes.length);
-  button.onclick = clearThisNote();
-  button.innerHTML = 'Delete';
-  newOnScreen.appendChild(button);
-  onScreen.appendChild(newOnScreen);
+  noteTally = 0;
+  $('#all-notes').empty();
+  allNotes.map((note) => {
+    let onScreen = document.getElementById('all-notes');
+    let newOnScreen = document.createElement('p');
+    newOnScreen.className = 'my-little-note';
+    newOnScreen.appendChild(document.createTextNode(note));
+    console.log(note);
+    let button = document.createElement('button');
+    button.className = 'btn btn-danger';
+    button.setAttribute('data-value', noteTally);
+    button.innerHTML = 'Delete';
+    button.onclick = clearThisNote();
+    newOnScreen.appendChild(button);
+    onScreen.appendChild(newOnScreen);
+    noteTally++;
+  });
 };
 
 getAllNotes = () => {
-  allNotes = localStorage.getItem(1).split(',');
-  console.log(allNotes[0]);
+  allNotes = localStorage.getItem(1) ? localStorage.getItem(1).split(',') : [];
   for (let i = 0; i < allNotes.length; i++) {
     let allScreen = document.getElementById('all-notes');
     let newAllScreen = document.createElement('p');
     newAllScreen.className = 'my-little-note';
     newAllScreen.appendChild(document.createTextNode(allNotes[i]));
     let button = document.createElement('button');
-    // button.id = 'app-button';
     button.className = 'btn btn-danger';
     button.setAttribute('data-value', i);
-    button.onclick = clearThisNote();
     button.innerHTML = 'Delete';
+    button.onclick = clearThisNote();
     newAllScreen.appendChild(button);
     allScreen.appendChild(newAllScreen);
   }
